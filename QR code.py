637dde11051604766902c517654c9dad7a800a1a
KrishithaDara <138888399+KrishithaDara@users.noreply.github.com>
2023-07-09 13:08:10 +0530

    Add files via upload

---
+++ QR code.py
@@ -0,0 +1,21 @@
+import qrcode
+
+
+def generate_qrcode(text):
+       qr=qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+
+            
+       )
+       qr.add_data(text)
+       qr.make(fit=True)
+       img=qr.make_image(fill_color="black",back_color="white")
+       img.save("YouTube.png")
+
+url=input("https://www.youtube.com/")  
+generate_qrcode(url) 
+# we can acess the YouTube by scanning the qrcode generated in the code
+
